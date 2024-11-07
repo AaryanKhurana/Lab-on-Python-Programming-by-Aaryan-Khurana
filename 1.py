4bad19ada16b5e0159be4cd81e14ffa4c7a1397a
AaryanKhurana <aaryankhran@gmail.com>
2024-11-07 22:09:14 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,2 @@
+print("Hello World")
+print("THIS PROGRAM IS WRITTEN BY AARYAN KHURANA ERP :- 0231BCA187")
---
+++ 21.py
@@ -0,0 +1,6 @@
+# Python program to demonstrate formatted or F string
+
+name = "Jagrit"
+age = 19
+print(f"Name: {name} , Age: {age}")
+print("THIS PROGRAM IS WRITTEN BY AARYAN KHURANA ERP :- 0231BCA187")
